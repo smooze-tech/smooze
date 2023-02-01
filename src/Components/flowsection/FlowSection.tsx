c279c22e2bcbd14cd3e57aa9b904ff3716a585b0
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2023-02-01 23:04:51 +0100

    redirect

--- src/Components/flowsection/FlowSection.tsx
+++ src/Components/flowsection/FlowSection.tsx
@@ -28,13 +28,13 @@ const mockData = [
     bottomtext:
       'Select multiple genres and moods to create the perfect soundtrack',
     para: 'Create your own perfect soundtrack',
-    img: '/src/assets/flowsection/Flowcover2.png',
+    img: '../../assets/flowsection/Flowcover2.png',
     children: <AddIcon />
   },
   {
     bottomtext: 'Select multiple artists to create the perfect sountrack',
     para: 'Create your own concert with your favorite singers',
-    img: '/src/assets/flowsection/Flowcover3.png',
+    img: '../../assets/flowsection/Flowcover3.png',
     children: <AddIcon />
   }
 ];
